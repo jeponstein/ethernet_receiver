fd106aeed393bee6e3721a8637c22104b475bed9
jeponstein <je.ponstein@gmail.com>
2025-04-10 22:35:22 +0200

    small modification

--- python_project/test.py
+++ python_project/test.py
@@ -1,5 +1,14 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-print(s.getsockname()[1])
-s.close()
+from multiprocessing import Process, Lock
+
+def f(l, i):
+    l.acquire()
+    try:
+        print('hello world', i)
+    finally:
+        l.release()
+
+if __name__ == '__main__':
+    lock = Lock()
+
+    for num in range(10):
+        Process(target=f, args=(lock, num)).start()
