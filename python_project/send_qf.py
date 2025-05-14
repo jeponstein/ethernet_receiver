d26a051ea0b6ce9a0829d86994d04a12b9a54ca2
jeponstein <je.ponstein@gmail.com>
2025-05-14 22:05:48 +0200

    last edits python, made function

---
+++ python_project/send_qf.py
@@ -0,0 +1,21 @@
+from socket import *
+
+dst_addr = b'\x00\x0a\x35\x00\x01\x02'
+checksum = b'\x00\x00\x00\x00'
+data=b'\x00'
+
+# MIGHT NEED TO BE RECONFIGURED DEPENDING ON HOST:
+# HOST NEEDS TO BE A LINUX MACHINE
+interface = "enp37s0"
+src_addr = b'\x00\xd8\x61\x2d\x29\x41'
+
+def sendPacket(factor):
+
+    factor = factor+1031
+    factor = factor.to_bytes(2, 'little')
+
+    s = socket(AF_PACKET, SOCK_RAW)
+    s.bind( (interface, 0) )
+    print("sent a packet")
+    s.send(dst_addr+src_addr+factor+data+checksum)
+
